e1ec5870ff9f661410e47133ede069e249134f19
unknown <vico_joseluis@hotmail.com>
2024-09-12 14:50:25 -0300

    agregando nombre

--- src/flow/welcome.flow.ts
+++ src/flow/welcome.flow.ts
@@ -4,6 +4,6 @@ import { BaileysProvider } from '@builderbot/provider-baileys'
 //Flujo de Bienvenida
 export const flowBienvenido = addKeyword<BaileysProvider, MemoryDB>(EVENTS.WELCOME) //este flujo se ejecuta cuando no se ingresa una palabra clave
 .addAction(async (ctx, { flowDynamic }) => {
-    await flowDynamic(`👋 Hola, bienvenido/a ${ctx.name} soy tu asistente virtual de la facultad de Humanidades y Ciencias Sociales de la Unju`)
+    await flowDynamic(`👋 Hola, bienvenido/a ${ctx.name} soy Huma, tu asistente virtual 🤖 de la facultad de Humanidades y Ciencias Sociales de la Unju`)
 })
 .addAnswer('✏ Escribí la palabra *menú*.')
